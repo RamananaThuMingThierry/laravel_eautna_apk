33a1526346ad4311e9aeaa0b97885fecf0f7949d
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2024-01-13 09:33:33 +0300

    Nous avons créer api route auth

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\MoisController;
+use App\Http\Controllers\PortesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+/** --------- Public Routes ---------- **/
+Route::post("/register", [AuthController::class, 'register']);
+Route::post("/login", [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/** --------- Protected Routes --------- **/
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    /** ---------- Users ---------- */
+    Route::get('/users', [AuthController::class, "show"]);
+    Route::put('/users', [AuthController::class, "update"]);
+    Route::post('/logout', [AuthController::class, "logout"]);
+
 });
