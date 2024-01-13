f562f5d441c1ba32bdf19a2d81a571e38909d10c
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2024-01-13 07:54:38 +0300

    Nous avons configurer ==> config/filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,30 @@ return [
             'throw' => false,
         ],
 
+        'users' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/users'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'membres' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/membres'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+        
+        'posts' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/posts'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
