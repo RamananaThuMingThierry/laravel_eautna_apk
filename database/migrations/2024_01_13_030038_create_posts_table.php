8f2a24e4b8b857e875b981895eb62426cbc4f4e5
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2024-01-16 11:01:57 +0300

    Mettre à jours la table posts

--- database/migrations/2024_01_13_030038_create_posts_table.php
+++ database/migrations/2024_01_13_030038_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('image')->nullable();
-            $table->string('description');
+            $table->text('description');
             $table->unsignedBigInteger('users_id');
             $table->foreign('users_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->date('date');
